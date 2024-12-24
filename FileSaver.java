70fd25164cb072b2c309c6eb466fe0fe4502111c
kiki <mrifqichusainis@gmail.com>
2024-12-24 16:09:59 +0700

    [UPDATE] Merubah struktur kelas pada FileSaver yang bertujuan untuk mengintegrasikan class menjadi child dari class FileHandler.

--- FileSaver.java
+++ FileSaver.java
@@ -1,40 +1,31 @@
 import javax.swing.*;
 import java.io.File;
 
-public class FileSaver {
-    private static String filePath;
-
-    public static String getSaveFilePath() {
-        // Membuat JFileChooser untuk memilih lokasi dan nama file
-        JFileChooser fileChooser = new JFileChooser();
+public class FileSaver extends FileHandler {
+    @Override
+    protected File handleFile() {
         fileChooser.setDialogTitle("Pilih lokasi dan nama untuk menyimpan file");
 
-        // Atur ekstensi file default sebagai PNG
-        fileChooser.setFileFilter(new javax.swing.filechooser.FileNameExtensionFilter("PNG Images", "png"));
-
         int userSelection = fileChooser.showSaveDialog(null);
-
         if (userSelection == JFileChooser.APPROVE_OPTION) {
-            File fileToSave = fileChooser.getSelectedFile();
+            file = fileChooser.getSelectedFile();
             // Pastikan file memiliki ekstensi ".png"
-            filePath = fileToSave.getAbsolutePath();
-            if (!filePath.toLowerCase().endsWith(".png")) {
-                filePath += ".png";
+            if (!file.getAbsolutePath().toLowerCase().endsWith(".png")) {
+                file = new File(file.getAbsolutePath() + ".png");
             }
-
-            return filePath;
+            return file;
         } else {
-            JOptionPane.showMessageDialog(null,
-                    "Proses penyimpanan dibatalkan.",
+            showMessage("Proses penyimpanan dibatalkan.",
                     "Gagal disimpan!",
                     JOptionPane.WARNING_MESSAGE);
-            return "";
+            return null;
         }
     }
-    public static void saveSukses(){
+
+    public void saveSukses(){
         // Tampilkan konfirmasi path yang dipilih
         JOptionPane.showMessageDialog(null,
-                "File berhasil disimpan di: " + filePath,
+                "File berhasil disimpan di: " + file.getAbsolutePath(),
                 "Berhasil disimpan!",
                 JOptionPane.INFORMATION_MESSAGE);
     }
