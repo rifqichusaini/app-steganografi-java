a0fc9f36cceb8e0a8a6024b64e3623871991a2f2
kiki <mrifqichusainis@gmail.com>
2024-12-24 16:10:34 +0700

    [UPDATE] Merubah struktur kelas pada FileChooser yang bertujuan untuk mengintegrasikan class menjadi child dari class FileHandler.

--- FileChooser.java
+++ FileChooser.java
@@ -3,30 +3,26 @@ import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class FileChooser {
-	public static File MakeFileChooser() {
-		JFileChooser fileChooser = new JFileChooser();
+public class FileChooser extends FileHandler {
+	@Override
+	protected File handleFile() {
 		fileChooser.setDialogTitle("Pilih lokasi gambar yang ingin dipilih");
-
-		// Tambahkan filter file untuk hanya menerima file PNG
-		FileNameExtensionFilter filter = new FileNameExtensionFilter("PNG Images", "png");
-		fileChooser.setFileFilter(filter);
 
 		int option = fileChooser.showOpenDialog(null);
 		if (option == JFileChooser.APPROVE_OPTION) {
-			File file = fileChooser.getSelectedFile();
-
-			// Validasi ekstensi file untuk memastikan hanya file PNG diterima
+			file = fileChooser.getSelectedFile();
+			// Validasi ekstensi file
 			if (file.getName().toLowerCase().endsWith(".png")) {
 				return file;
 			} else {
-				// Tampilkan pesan kesalahan jika file tidak valid
-				JOptionPane.showMessageDialog(null, "Hanya file PNG yang diperbolehkan.",
-						"Error", JOptionPane.ERROR_MESSAGE);
+				showMessage("Hanya file PNG yang diperbolehkan.",
+						"Error",
+						JOptionPane.ERROR_MESSAGE);
 			}
 		} else {
-			JOptionPane.showMessageDialog(null, "Tidak ada file yang dipilih.",
-					"Informasi", JOptionPane.INFORMATION_MESSAGE);
+			showMessage("Tidak ada file yang dipilih.",
+					"Informasi",
+					JOptionPane.INFORMATION_MESSAGE);
 		}
 		return null;
 	}
